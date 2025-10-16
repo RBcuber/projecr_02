263809819b0782749ac00fe3f7d9f8be8b1b7da6
RBcuber <igor.kirichenko.07@gmail.com>
2025-10-17 00:03:12 +0200

    add hw2

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import Goodbye from "./components/Goodbye/Goodbye";
 import Hello from "./components/Hello/Hello";
 import PersonalGreeting from "./components/PersonalGreeting/PersonalGreeting";
 import ProductCard from "./components/ProductCard/ProductCard";
+import ProfileCard from "./components/ProfileCard/ProfileCard";
+import Tools from "./components/Tools/Tools";
 
 function App() {
   return (
     <>
       <p>Hello!</p>
       <Hello />
+      <div>
+        <Tools />
+      </div>
       <Goodbye />
       <PersonalGreeting name="Ihor" />
       <PersonalGreeting name="Ne Ihor" />
@@ -16,8 +21,14 @@ function App() {
         title="Opaeroo Paarungsspielzeug für Hunde"
         image="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcT6dCWJhg_NWxlD6zuYn_a0CDMkebqD3IWtGcPHu6ZOhKs5KmI9eb9c6W41D6RlFy1PzLVs8pTI-JJDLThOPMEAbQ99Pg6ve69oNvUt2q6acOtAv5sXHBjIKvCMBLNNfxjN5sNTGIm3NQ&usqp=CAc"
         price={86.99}
-
       ></ProductCard>
+      <div className="profile-card">
+        <ProfileCard
+        avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS6SdDSDRVS_1-JYWyuSWOF0u3OdZMdzbwNA&s"
+        name="Ihor"
+        description="Кодю, пью кофе, смотрю Аниме и мечтаю запустить свой проект. Люблю React, котов и D&D."
+        ></ProfileCard>
+      </div>
     </>
   );
 }
---
+++ src/components/Tools/Tools.tsx
@@ -0,0 +1,5 @@
+function Tools() {
+  return <p>Important step for our journey</p>;
+}
+
+export default Tools;
